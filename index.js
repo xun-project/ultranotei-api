fa8c4b83dc425d35a8628120cf4d3ae772cf878d
UltraNote <support@ultranote.org>
2023-10-02 18:13:53 +0200

    split integrated

--- index.js
+++ index.js
@@ -188,6 +188,13 @@ XUNI.prototype.createIntegrated = function (address, paymentId) {
     });
 };
 
+XUNI.prototype.splitIntegrated = function (address) {
+return new Promise((resolve, reject) => {
+if (isUndefined(address) || !isIntAddress(address)) reject('address' + err.intAddr);
+wrpc(this, 'createIntegrated', { integrated_address: address }, resolve, reject);
+   });
+};
+
 XUNI.prototype.deleteAddress = function (address) {
   return new Promise((resolve, reject) => {
     if (isUndefined(address) || !isAddress(address)) reject('address' + err.addr);
